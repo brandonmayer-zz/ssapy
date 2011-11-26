85ed9202521b3c99fd7a9a1d7218b4e23ddd7039
Brandon Mayer <b.mayer1@gmail.com>
2011-11-26 14:01:53 -0500

    wrappers for point and distribution predictions and a unit test of their binary pickle IO

---
+++ courses/fall2011/csci2951/auctionSimulator/hw4/pricePrediction/pointSCPP.py
@@ -0,0 +1,85 @@
+import pickle
+import numpy
+
+class pointSCPP(object):
+    """
+    Class wrapper for point self confirming price predictions
+    """
+    def __init__(self, pointPricePrediction = None):
+        
+        if isinstance(pointPricePrediction,numpy.ndarray):
+            if numpy.atleast_2d(pointPricePrediction).shape[0] == 1:
+                self.data = pointPricePrediction
+                self.m = numpy.atleast_2d(self.data).shape[1]
+            else:
+                print "----Warning----"
+                print "pointSCPP.__init__"
+                print "pointPricePrediction must be a 1d array"
+                print "self.data = None"
+                self.data = None
+        else:
+            print "----Warning----"
+            print "pointSCPP.__init__"
+            print "pointPricePrediction must be a numpy.ndarray"
+            print "self.data = None"
+            self.data = None
+                    
+    @staticmethod
+    def type():
+        return "pointSCPP"
+    
+    def savePickle(self,f):
+        """
+        Function to serialize class to binary file
+        """
+        if isinstance(f,basestring):
+            pickle.dump(self,open(f,'wb'))
+            
+        elif isinstance(f,file):
+            pickle.dump(self,f)
+        else:
+            print "----ERROR----"
+            print "{0}::savePickle".format(self.type())
+            print "Cannot save pickled instance"
+            print "Must Provide filename string or file object"
+            return False
+        
+    def loadPickle(self,f):
+        """
+        Function to load data from serialized class instance
+        """
+        if isinstance(f,basestring):
+            
+            temp = pickle.load(open(filename,'rb'))
+            if temp.type() == self.type():
+                self.data   = temp.data
+                self.m      = temp.m
+            else:
+                print "----Warning----"
+                print "{0}::loadPickle".format(self.type())
+                print "Specified file is not compatible with instance type."
+                print "Load Failed"
+                return False
+            
+        elif isinstance(f,file):
+            
+            temp = pickle.load(f)
+            if temp.type() == self.type():
+                self.data   = temp.data
+                self.m      = temp.m
+            else:
+                print "----Warning----"
+                print "{0}::loadPickle".format(self.type())
+                print "Specified file is not compatible with instance type."
+                print "Load Failed"
+                return False
+            
+        else:
+            print "----ERROR----"
+            print "{0}::loadPickle".format(self.type())
+            print "Cannot load pickled instance"
+            print "Must Provide filename string or file object"
+            return False
+            
+                
+    
---
+++ courses/fall2011/csci2951/auctionTests/hw4/testPredictionIO.py
@@ -0,0 +1,90 @@
+"""
+this is /auctionTests/testPredictionIO.py
+
+Author:    Brandon A. Mayer
+Date:      11/26/2011
+
+Just some IO sanity checks.
+"""
+from auctionSimulator.hw4.pricePrediction.marginalDistributionSCPP import *
+from auctionSimulator.hw4.pricePrediction.pointSCPP import *
+
+import numpy
+import unittest
+import tempfile
+class TestPredictionIO(unittest.TestCase):
+    
+    def setUp(self):
+        """
+        Initialize some random distributions that will act as test data for 
+        Self Confirming Price Predictions.
+        """
+        #make some fake data
+        self.m = 5
+        self.randomPriceVector = numpy.random.random_integers(1,10,self.m)
+        
+        mu = [5,3,2,1,1]
+        sigma = [.2]*self.m
+        self.randomPriceDist = []
+        for good in xrange(self.m):
+            randomPrices = numpy.random.normal(loc=mu[good],scale=sigma[good],size=10000)
+            self.randomPriceDist.append(numpy.histogram(randomPrices,bins=range(0,51),density=True))
+            
+        #make some temporary files
+        self.pointFileObject = tempfile.NamedTemporaryFile('w+b',suffix='.pkl')
+        self.distFileObject = tempfile.NamedTemporaryFile('w+b',suffix='.pkl') 
+        
+    def test_io(self):
+        
+        # create the prediction instance wrappers
+        pointPrediction = pointSCPP(self.randomPriceVector)
+        distPrediction  = marginalDistributionSCPP(self.randomPriceDist)
+                
+        # save them to temp files
+        pointPrediction.savePickle(self.pointFileObject.file)
+        distPrediction.savePickle(self.distFileObject.file)
+        
+        #move to begginning for reading
+        self.pointFileObject.file.seek(0)
+        self.distFileObject.file.seek(0)
+        
+        # reload the pickled instances
+        reloadedPointPrediction = pickle.load(self.pointFileObject)
+        reloadedDistPrediction = pickle.load(self.distFileObject)
+        
+        # check the reloaded point prediction is the correct class instance
+        self.assertIsInstance(reloadedPointPrediction, pointSCPP)
+        
+        # check the reloaded distribution prediction is the correct class instance
+        self.assertIsInstance(reloadedDistPrediction, marginalDistributionSCPP)
+        
+        # check the reloaded distribution represents marginal distributions over the correct
+        # number of goods
+        self.assertEqual( len(reloadedDistPrediction.data), self.m )
+        
+        # check the reloaded and original distribution instances have the same 
+        # number of distributions
+        self.assertEqual( len(reloadedDistPrediction.data), len(distPrediction.data) )
+        
+        # check the reloaded and original price vectors are of the same size
+        self.assertEqual( reloadedPointPrediction.data.shape, pointPrediction.data.shape )
+        
+         
+        # check all datum are approximatly (to decimal places) the same for the reloaded point price prediction            
+        self.assertEqual( list(reloadedPointPrediction.data), list(pointPrediction.data) )
+        
+        for idx in xrange(len(reloadedDistPrediction.data)):
+            histR, binEdgesR = reloadedDistPrediction.data[idx]
+            histO, binEdgesO = distPrediction.data[idx]
+            
+            # test that the reloaded and original histogram count arrays have the same shape
+            self.assertEqual(histR.shape,histO.shape)
+            
+            # test that the reloaded and original histogram counts have the same data
+            self.assertEqual(list(histR), list(histO))
+            
+            # test that the reloaded and original bin edge arrays have the same data
+            self.assertEqual(list(binEdgesR), list(binEdgesO))
+   
+if __name__ == "__main__":
+    unittest.main()
