88540febb8de2b50c925871dcf6f5b5f28e81697
Brandon Mayer <b.mayer1@gmail.com>
2011-11-27 15:24:01 -0500

    initial pricePredictionAgent.py commit

---
+++ courses/fall2011/csci2951/auctionSimulator/hw4/agents/pricePredictionAgent.py
@@ -0,0 +1,9 @@
+"""
+This is /auctionSimulator/hw4/agents/pricePredicitonAgent
+
+Author:        Brandon A. Mayer
+Date:          11/26/2011
+
+A base class for agents who participate in simultaneous auctions described by
+Yoon & Wellman 2011 and use price prediction strategies.
+"""
