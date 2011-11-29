02e9dfb9f58599162ce2c8449e975eea3abc4bd0
Brandon Mayer <b.mayer1@gmail.com>
2011-11-28 23:43:51 -0500

    took out old commented code from testDistAgents.py

--- courses/fall2011/csci2951/auctionTests/hw4/testDistAgents.py
+++ courses/fall2011/csci2951/auctionTests/hw4/testDistAgents.py
@@ -72,40 +72,7 @@ class testDistAgents(unittest.TestCase):
         myStraightMU.printSummary()
         print''
         
-
-    
-        
-#def main():
-#    
-#    #simulate a price Distribution
-#    m = 5
-#    muPrice = [5,3,2,1,1]
-#    sigma = [.2]*m
-#    priceDistribution = []
-#    for good in xrange(m):
-#        randomPrices = numpy.random.normal(loc=muPrice[good],scale=sigma[good],size=10000)
-#        priceDistribution.append(numpy.histogram(randomPrices,bins=range(0,51),density=True))
-#        
-#    myTargetMU = targetMU()
-#    myTargetMU.printAllSummary({'priceDistribution':priceDistribution})
-#    print "\n\n"
-#    
-#    myTargetMUS = targetMUS()
-#    myTargetMUS.printAllSummary({'priceDistribution':priceDistribution})
-#    print "\n\n"
-#    
-#    myStraightMU = straightMU()
-#    myStraightMU.printAllSummary({'priceDistribution':priceDistribution})
-#    print "\n\n"
-#    
-#    myAverageMU = averageMU()
-#    myAverageMU.printAllSummary({'priceDistribution':priceDistribution})
-#    print "\n\n"
-#    
-#    myRiskAware = riskAware(A = 2)
-#    myRishAware.printAllSummary({'priceDistribution':priceDistribution})
-#    print "\n\n"
-    
+  
         
 if __name__ == "__main__":
     unittest.main()
