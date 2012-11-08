52473f60f5f83af785bf2c683202810d387ac722
bam593 <b.mayer1@gmail.com>
2012-11-08 16:30:11 -0500

    updates

--- ssapy/pricePrediction/dok_hist.py
+++ ssapy/pricePrediction/dok_hist.py
@@ -29,8 +29,14 @@ class dok_hist(object):
                 raise ValueError("Must specify number of dimentions - m")
             self.bins = numpy.asarray([numpy.concatenate(([0], numpy.arange(0,51,1)))\
                                        for i in xrange(m)])
+        else:
+            raise NotImplemented("Not Yet!")
             
         self.counts = {}
+        
+        self.isnormed = False
+        
+        self.accum = 0.
         
     def extent(self):
         return [(bins[0], bins[1]) for bins in self.bins]
@@ -39,24 +45,47 @@ class dok_hist(object):
         return self.bins.shape[0]
     
     def range_from_val(self, val):
-        key = self.key_from_val(val)
-        return self.range_from_key(key)
+        
+        aval = numpy.atleast_1d(val)
+        
+        if not aval.shape[0] == self.bins.shape[0]:
+            raise ValueError("val.shape[0] = {0} != bins.shape[0] = {1}".\
+                             format(aval.shape[0], self.bins.shape[0]))
+            
+        r = []
+        for i, v in enumerate(aval):
+            if v < self.bins[i][0] or v > self.bins[i][-1]:
+                raise ValueError("{0} out of range".format(v))
+            
+            greater_bins = v <= self.bins[i]
+            lower_bin_index = greater_bins[0]
+            upper_bin_index = greater_bins[1]
+            r.append(tuple(lower_bin_index, upper_bin_index))
+        
+        return r     
+        
                 
     def range_from_key(self,key):
         r = []
         for i, k in enumerate(key):
-            r.append((self.bins[i,k-1],self.bins[i,k]))
+            if k == 0:
+                r.append((0,0))
+            else:
+                r.append((self.bins[i,k-1],self.bins[i,k]))
         
         return r
+    
+    def bin_center_from_key(self,key):
+        r = numpy.atleast_2d(self.range_from_key(key))
+        bin_center = r[:,0] + (numpy.diff(r).flatten())*.5
+        return bin_center 
         
     def key_from_val(self, val):
         
-        if isnumber(val):
-            aval = numpy.asarray([val])
-        else:
-            aval = numpy.asarray(val)
+        aval = numpy.atleast_1d(val)
             
         if not aval.shape[0] == self.bins.shape[0]:
+            pass
             raise ValueError("val.shape[0] = {0} != bins.shape[0] = {1}".\
                              format(aval.shape[0], self.bins.shape[0]))
             
@@ -76,9 +105,20 @@ class dok_hist(object):
             
         return ret
             
+    def delta_from_val(self, val):
+        """
+        return the area of the bin in which the value falls
+        """
+        ranges = self.range_from_val(val)
+        
+        return numpy.diff(ranges).flatten()
     
+    def bin_areal_from_val(self,val):
+        return numpy.prod(self.delta_from_val(val))
+            
+        
     def upcount(self, val, mag = 1.0):
-        key = self.key_from_val(numpy.asarray(val))
+        key = self.key_from_val(numpy.atleast_1d(val))
         
         key = tuple(key) 
         
@@ -86,21 +126,98 @@ class dok_hist(object):
             self.counts[key] = mag
         else:
             self.counts[key] += mag    
+            
+        self.isnormed = False
+        
+        self.accum += (mag*self.bin_areal_from_val(val))
+        
+    def recompute_norm(self):
+        self.accum = 0.0
+        
+        for val in self.counts.values():
+            self.accum += val*self.bin_areal_from_val(val)
+        
+        self.isnormed = False
+            
+        return self.accum
     
+    def norm_const(self):
+        """
+        Return the normalization constant
+        """
+        return self.accum
+    
+    def normalize(self,recompute_norm = False):
+        """
+        Normalize the distribution and 
+        return the normalization constant
+        """
+        if recompute_norm:
+            self.recompute_norm()
+            
+        for val in self.counts.values():
+            val/=self.accum
+            
+        self.isnormed = True
+    
+        return self.accum
+    
+    def eval(self, val, norm = True, recompute_norm = False):
+        
+        key = tuple(self.key_from_val(numpy.asarray(val)))
+        
+        if not key in self.counts.keys():
+            return 0.
+        
+        if recompute_norm:
+            self.recompute_norm()
+        
+        if norm and not self.isnormed:    
+            return self.counts[key]/self.accum
+        else:
+            return self.counts[key]
+        
+    def sample(self, n_samples = 1, recompute_norm = False):
+        p = numpy.zeros(len(self.counts.keys()))
+        
+        if recompute_norm:
+            self.recompute_norm()
+            
+        if self.isnormed:
+            for i, v in enumerate(self.counts.values()):
+                p[i] = v
+        else:
+            for i, k in enumerate(self.counts.key()):
+                pass
+                
+            
+            
+        
+        
+        
+        
 def main():
-    hist = dok_hist(m=1)
-    k  = hist.key_from_val(4)
-    print k
-    r = hist.range_from_val(4)
-    print r
-    
-    print hist.range_from_val(4.5)
-    print hist.range_from_val(50)
+    hist = dok_hist(m=2)
+    r = hist.range_from_val([0,0])
+    hist.upcount([30,30],6)
+    hist.upcount([20,30],10)
+    samples = hist.sample(5)
     pass
-#    hist.upcount([1,2],5)
-#    print'{0}'.format(hist.range_from_val([1,2]))
-#    pass
-#    pass
+    
+        
+#    hist = dok_hist(m=1)
+        
+#    hist.upcount(4.5,3)
+#    hist.upcount(50,10)
+#    hist.upcount(10,4)
+    
+
+#    print hist.pmf(4)
+#    print hist.pmf(4.1)
+#    print hist.pmf(10)
+#    print hist.pmf(50)
+    
+
 
 if __name__ == "__main__":
     main()
