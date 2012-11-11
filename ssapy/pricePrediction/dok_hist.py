73bd020be7ee760b36f94304828a9d365e163a94
brandonmayer <b.mayer1@gmail.com>
2012-11-11 16:02:27 -0500

    implemented binary search for bin ranges. Made keys of counts dictionary ranges instead of bin indices.

--- ssapy/pricePrediction/dok_hist.py
+++ ssapy/pricePrediction/dok_hist.py
@@ -27,69 +27,98 @@ class dok_hist(object):
             m = kwargs.get('m')
             if m == None:
                 raise ValueError("Must specify number of dimentions - m")
-            self.bins = numpy.asarray([numpy.concatenate(([0], numpy.arange(0,51,1)))\
-                                       for i in xrange(m)])
+#            self.bins = numpy.asarray([numpy.concatenate(([0], numpy.arange(0,51,1)))\
+#                                       for i in xrange(m)])
             
-        self.counts = {}
+            self.bins = []
+            for i in xrange(m):
+                self.bins.append([0])
+                for j in xrange(0,51):
+                    self.bins[i].append(j)
+                    
+        self.c = {}
         
     def extent(self):
-        return [(bins[0], bins[1]) for bins in self.bins]
+        return [(bins[0], bins[-1]) for bins in self.bins]
     
     def dim(self):
-        return self.bins.shape[0]
+        return len(self.bins)
     
     def range_from_val(self, val):
-        key = self.key_from_val(val)
-        return self.range_from_key(key)
-                
-    def range_from_key(self,key):
+        """
+        Search for the range of the bins corresponding to val
+        in each dimension using a binary search.
+        """
+        aval = numpy.atleast_1d(val)
+        
+        if(aval.shape[0] != self.dim()):
+            raise ValueError("val.shape[0] = {0} != self.dim() = {1}".\
+                             format(aval.shape[0], self.dim()))
+            
         r = []
-        for i, k in enumerate(key):
-            r.append((self.bins[i,k-1],self.bins[i,k]))
         
+        #loop over all dimensions
+        for dim_idx, bin_list in enumerate(self.bins):
+            
+            v = aval[dim_idx]
+            
+            #bounds check
+            if v < bin_list[0]:
+                raise ValueError("v = {0} < bin_list[0][0] = {1}".format(v, bin_list[0][0]))
+            
+            if v > bin_list[-1]:
+                raise ValueError("v = {0} > bin_list[-1][1] = {1}".format(v, bin_list[-1][0]))
+            
+            first = 0
+            last  = len(bin_list)
+            while True:
+                
+                mid = int((last-first)/2)
+                
+                if first >= last:
+                    raise ValueError("No bin found for v = {0} in\nbins = {1}".format(v, bin_list))
+                
+                if bin_list[mid] < v and v <= bin_list[mid+1]:
+                    break
+                elif (bin_list[mid] == bin_list[mid+1]) and v == bin_list[mid]:
+                    #This condition allows for bins of 0 width;
+                    #counting exact values instead of ranges.
+                    break
+                else:
+                    if v > bin_list[mid]:
+                        first = mid
+                    else:
+                        last = mid  
+                               
+            r.append( (bin_list[mid], bin_list[mid+1]) )
+            
         return r
-        
-    def key_from_val(self, val):
-        
-        if isnumber(val):
-            aval = numpy.asarray([val])
-        else:
-            aval = numpy.asarray(val)
-            
-        if not aval.shape[0] == self.bins.shape[0]:
-            raise ValueError("val.shape[0] = {0} != bins.shape[0] = {1}".\
-                             format(aval.shape[0], self.bins.shape[0]))
-            
-        ret = numpy.zeros(aval.shape, dtype=numpy.int)
-        
-        if numpy.all(aval == 0):
-            return ret
-            
-        for i in xrange(aval.shape[0]):
-            if aval[i] < self.bins[i][0] or aval[i] > self.bins[i][-1]:
-                raise ValueError("{0} out of range".format(aval))
-            
-            greater_bins = int(numpy.nonzero( aval[i] <= self.bins[i])[0][0])
-            
-            #the first bin greater than the value
-            ret[i] = greater_bins
-            
-        return ret
-            
-    
+                 
     def upcount(self, val, mag = 1.0):
-        key = self.key_from_val(numpy.asarray(val))
+        r = tuple(self.range_from_val(numpy.atleast_1d(val)))
         
-        key = tuple(key) 
+        try:
+            self.c[r] += mag
+        except KeyError:
+            self.c[r] = mag
+                
+    def counts(self, val):
+        r = tuple(self.range_from_val(numpy.atleast_1d(val)))
         
-        if not key in self.counts.keys():
-            self.counts[key] = mag
+        c = self.c.get(r)
+        
+        if c == None:
+            return 0
         else:
-            self.counts[key] += mag    
+            return c
+        
     
 def main():
     hist = dok_hist(m=1)
-    key = hist.key_from_val(0)
+#    key = hist.key_from_val(0)
+    val = 0
+    r = hist.range_from_val(val) 
+    print 'r = {0}'.format(r)
 
 if __name__ == "__main__":
     main()
