acad3a41e40a09e5d1f0b1052949c31ffe846a0e
b.mayer1@gmail.com <b.mayer1@gmail.com>
2013-01-03 16:06:19 -0500

    updates

--- ssapy/strategies/condLocalBid.py
+++ ssapy/strategies/condLocalBid.py
@@ -30,7 +30,7 @@ def condLocalUpdate(bundles, revenue, bids, targetBid, samples, verbose = False)
         
     OUTPUTS
     -------
-        ubids       := (1d array like) updated bid vector
+        newBid     := (float) the new bid for the target good
     """
     
     newBid = 0.0
@@ -40,13 +40,13 @@ def condLocalUpdate(bundles, revenue, bids, targetBid, samples, verbose = False)
     normWon = numpy.count_nonzero(goodsWon[:,targetBid] == True)
     normLost = numpy.count_nonzero(goodsWon[:,targetBid] == False)
     
-    posIdxList = bundles[:,targetBid] == True
+    posIdxList = numpy.flatnonzero(bundles[:,targetBid] == True)
     
     for posIdx in posIdxList:
         posBundle = bundles[posIdx,:]
         negBundle = posBundle.copy()
         negBundle[targetBid] = False
-        negIdx = numpy.where((bundles == negBundle).all(axis=1))[0]
+        negIdx = numpy.where((bundles == negBundle).all(axis=1))[0][0]
         
         p1 = numpy.count_nonzero( numpy.all(goodsWon == posBundle, 1) ) + 1
         p1 = numpy.float(p1) / (normWon + 2)
@@ -58,7 +58,7 @@ def condLocalUpdate(bundles, revenue, bids, targetBid, samples, verbose = False)
             raise ValueError("p1 = {0} < 0.0".format(p1))
         
         p0 = numpy.count_nonzero( numpy.all(goodsWon == negBundle, 1) ) + 1 
-        p0 = numpy.float(p0) / ( normLost +  1 )
+        p0 = numpy.float(p0) / ( normLost +  2 )
         
         if p0 > 1.0:
             raise ValueError ("p0 = {0} > 1.0".format(p1))
@@ -69,9 +69,100 @@ def condLocalUpdate(bundles, revenue, bids, targetBid, samples, verbose = False)
     
         newBid += (revenue[posIdx]*p1) - (revenue[negIdx]*p0)
         
-    obid = bids.copy()
+    return newBid
+
+def condLocal(bundles, revenue, initialBids, samples, maxItr = 100, tol = 1e-5, verbose = False):
+    """
+    Starting form an initial bid, run the condLocal algorithm and return 
+    an updated bid vector.
     
-    obid[targetBid] = newBid
+    Use the following convention:
+        Iteration - consists of m updates where m is the number of goods
+        Update - an update for a single bid.
     
-    return obid
+    INPUTS
+    ------
+    bundles     := (2d array-like) List of bundles
+        
+    revenue     := (1d array-like) List of revenue (1:1 correspondence with bundles)
+    
+    initialBids := (1d array-like) List of initial bids, 1 per good at auction.
+                    bundles.shape[1] = initialBids.shape[0]
+                    
+    samples     := (2d array-like; shape = (nSamples x nGoods)) A list of samples from a 
+                    price distribution.
+                    
+    maxItr      := (int) Stop updating after maxItr iterations. If maxItr is reached,
+                    the algorithm is said to have diverged.
+    
+    tol         := (float) If the Euclidean distance between iterations 
+                    (an update has been performed to each of the m bids)
+                    is less than tol, the algorithm is said to have converged 
+                    and returns the updated bid vector.
+    
+    OUTPUTS
+    -------
+    obids        := (1d array-like) A list of bids - one for each good.
+    
+    converged    := (boolean) A flag indicating convergence.
+    
+    itr          := The number of Iterations (impying n*iterations updates were performed)
+                    performed. If converged = False -> itr = maxItr.
+                    
+    l            := Euclidean distance between the last update step 
+                    and the previous state of the bid vector.
+    """
+    m = bundles.shape[1]
+    obids = numpy.atleast_1d(initialBids).copy()
+    converged = False
+    
+    for itr in xrange(maxItr):
+        oldBid = obids.copy()
+        
+        for gIdx in xrange(m):
+            obids[gIdx] = condLocalUpdate(bundles, revenue, obids, gIdx, samples, verbose)
+        
+        l = numpy.dot(oldBid - obids, oldBid - obids)
+        
+        if l <= tol:
+            converged = True
+            break
+        
+    return obids, converged, itr, l   
+
+def plotCondLocal(bundles, revenue, initialBids, samples, maxItr, tol, filename = None, verbose = True):
+    import matplotlib.pyplot as plt
+    
+    GREY = numpy.atleast_1d([169.,169.,169.])/255
+    RED  = numpy.atleast_1d([255,0,0])/255
+    
+    m = bundles.shape[1]
+    
+    if (m != 2):
+        raise KeyError("Can only visualize updates for auctions with 2 goods")
+    
+    obids = numpy.atleast_1d(initialBids).copy()
+    converged = False
+    
+    plt.scatter(samples[:,0],samples[:,1], alpha=0.75)
+    
+    for itr in xrange(maxItr):
+        oldBid = obids.copy()
+        
+        plt.plot(oldBid[0],oldBid[1],color=GREY)
+        
+        for gIdx in xrange(m):
+            obids[gIdx] = condLocalUpdate(bundles, revenue, obids, gIdx, samples, verbose)
+            
+        plt.plot(obids[0],obids[1],color=RED)
+            
+        l = numpy.dot(oldBid - obids, oldBid - obids)
+        
+        if l <= tol:
+            converged = True
+            break
+    
+    plt.show()
+        
+    return obids, converged, itr, l  
     
---
+++ ssapy/strategies/tests/test_condLocalBid.py
@@ -0,0 +1,49 @@
+import unittest
+import numpy
+
+from ssapy.strategies.condLocalBid import condLocalUpdate, condLocal, plotCondLocal
+from ssapy import listBundles, msListRevenue
+
+
+class test_condLocalBid(unittest.TestCase):
+    def setUp(self):
+        self.samples2d = numpy.atleast_2d([[1,1],   [20,3], [10,7], [15,5],
+                                           [10,30], [15,26],[17,33], [2,40],
+                                           [30,30], [27,42],[30,38], [29,40],
+                                           [42,20], [38,7], [40,15], [33,10]])
+        
+        self.bundles2d = listBundles(2)
+        self.l2d = 1
+        self.v2d = [20,10] 
+        self.revenue2d = msListRevenue(self.bundles2d, self.v2d, self.l2d)
+    
+    def test_condLocalUpdate(self):
+        ibids = [25.,25.]
+        print ibids
+        targetBid = 0
+        ibids[targetBid] = condLocalUpdate(self.bundles2d, self.revenue2d, 
+                                           ibids, targetBid, self.samples2d, True)
+        
+        print ibids
+        
+    def test_condLocal(self):
+        ibids = [25.,25.]
+        print ibids
+        newBids,converged,itr,l = condLocal(self.bundles2d, self.revenue2d, ibids, 
+                                            self.samples2d, 100, 0.001, True)
+        
+        print newBids
+        print converged
+        print itr
+        print l
+        
+    def test_plotCondLocal(self):
+        ibids = [25.,25.]
+        
+        plotCondLocal(self.bundles2d, self.revenue2d, ibids, 
+                      self.samples2d, 100, 0.001, True)
+        
+        
+    
+if __name__ == "__main__":
+    unittest.main()
