0d5e6881bb36140a3442541bc034ef1eb557fc8a
b.mayer1@gmail.com <b.mayer1@gmail.com>
2013-01-03 11:59:45 -0500

    updates

--- ssapy/strategies/targetMVS.py
+++ ssapy/strategies/targetMVS.py
@@ -38,7 +38,7 @@ def targetMVS(bundles, revenue, pricePrediction, verbose = False):
 
 if __name__ == "__main__":
     from ssapy.util import listBundles
-    from ssapy.marketSchedule import listRevenue
+    from ssapy.agents.marketSchedule import listRevenue
     
     pp = [5,5]
     l = 1
