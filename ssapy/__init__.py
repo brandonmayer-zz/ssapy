ab5afcb7824f0404c0f9b445e5d191bf6659e5f7
b.mayer1@gmail.com <b.mayer1@gmail.com>
2013-01-03 12:54:45 -0500

    made interface to common functions for easier terminal in terminal.

--- ssapy/__init__.py
+++ ssapy/__init__.py
@@ -5,6 +5,13 @@ from .pricePrediction.dokHist import dokHist
 
 from ssapy.strategies import strategyDict
 
+from ssapy.agents.marketSchedule import listRevenue as msListRevenue
+
+from ssapy.agents.marketSchedule import \
+    randomValueVector as msRandomValueVector
+    
+from ssapy.util import listBundles
+
 #from ssapy.pricePrediction.dok_hist import dokDhist
 
 def getStrategy(ss):
