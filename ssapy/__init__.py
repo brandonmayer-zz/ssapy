071bfcfeb7442edfdd17bd327ddd90e428a2ccbc
b.mayer1@gmail.com <b.mayer1@gmail.com>
2013-01-04 13:08:57 -0500

    commenting out deperciated functions.

--- ssapy/__init__.py
+++ ssapy/__init__.py
@@ -3,7 +3,7 @@ import itertools
 #from pricePrediction.dok_hist import dok_hist
 from .pricePrediction.dokHist import dokHist
 
-from ssapy.strategies import strategyDict
+#from ssapy.strategies import strategyDict
 
 from ssapy.agents.marketSchedule import listRevenue as msListRevenue
 
@@ -14,13 +14,13 @@ from ssapy.util import listBundles
 
 #from ssapy.pricePrediction.dok_hist import dokDhist
 
-def getStrategy(ss):
-    strategy = strategyDict.get(ss)
-    
-    if strategy == None:
-        raise ValueError("Unknown Strategy - {0}".format(strategy))
-    
-    return strategy
+#def getStrategy(ss):
+#    strategy = strategyDict.get(ss)
+#    
+#    if strategy == None:
+#        raise ValueError("Unknown Strategy - {0}".format(strategy))
+#    
+#    return strategy
     
 
 
