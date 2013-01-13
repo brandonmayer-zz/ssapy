9f256fbeee1cfb5303c239aa5c2a86bea9319608
b.mayer1@gmail.com <b.mayer1@gmail.com>
2013-01-13 16:56:35 -0500

    added straightMUa

--- ssapy/agents/marketSchedule/straightMU.py
+++ ssapy/agents/marketSchedule/straightMU.py
@@ -2,6 +2,21 @@ from ..marketSchedule import listRevenue
 from .msAgent import msAgent
 from ...strategies.straightMU import straightMU as strategies
 
+class straightMUa(msAgent):
+    def __init__(self, **kwargs):
+        super(straightMUa, self).__init__(**kwargs)
+        
+    def bid(self, **kwargs):
+        pricePrediction = kwargs.get('pricePrediction')
+        
+        bundles = kwargs.get('bundles', self.listBundles())
+        
+        revenue = kwargs.get('revenue', listRevenue(bundles, self.v, self.l))
+        
+        verbose = kwargs.get('verbose',False)
+        
+        return strategies.straightMUa(bundles, revenue, pricePrediction, verbose)
+                                     
 class straightMU8(msAgent):
     def __init__(self, **kwargs):
         super(straightMU8, self).__init__(**kwargs)
