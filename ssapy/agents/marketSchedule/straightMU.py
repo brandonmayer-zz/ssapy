dd8dbfe59b98a84bec2160963ce489c18c1d8e2e
b.mayer1@gmail.com <b.mayer1@gmail.com>
2013-01-13 14:34:14 -0500

    relative imports

--- ssapy/agents/marketSchedule/straightMU.py
+++ ssapy/agents/marketSchedule/straightMU.py
@@ -1,6 +1,6 @@
-from ssapy.agents.marketSchedule import msAgent, listRevenue
-import ssapy.strategies.straightMU as strategies
-from ssapy.util import listBundles
+from ..marketSchedule import listRevenue
+from .msAgent import msAgent
+from ...strategies.straightMU import straightMU as strategies
 
 class straightMU8(msAgent):
     def __init__(self, **kwargs):
@@ -10,7 +10,7 @@ class straightMU8(msAgent):
         
         pricePrediction = kwargs.get('pricePrediction')
         
-        bundles = kwargs.get('bundles', listBundles(self.m))
+        bundles = kwargs.get('bundles', self.listBundles())
         
         revenue = kwargs.get('revenue', listRevenue(bundles, self.v, self.l))
         
@@ -26,7 +26,7 @@ class straightMU64(msAgent):
     def bid(self, **kwargs):
         pricePrediction = kwargs.get('pricePrediction')
         
-        bundles = kwargs.get('bundles', listBundles(self.m))
+        bundles = kwargs.get('bundles', self.listBundles())
         
         revenue = kwargs.get('revenue', listRevenue(bundles, self.v, self.l))
         
@@ -41,7 +41,7 @@ class straightMU256(msAgent):
     def bid(self, **kwargs):
         pricePrediction = kwargs.get('pricePrediction')
         
-        bundles = kwargs.get('bundles', listBundles(self.m))
+        bundles = kwargs.get('bundles', self.listBundles())
         
         revenue = kwargs.get('revenue', listRevenue(bundles, self.v, self.l))
         
