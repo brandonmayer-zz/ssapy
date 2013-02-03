ee91d36c49a85d05462373f125f4939699094d2f
b.mayer1@gmail.com <b.mayer1@gmail.com>
2013-02-03 17:17:25 -0500

    added `

--- exe/jtc.sh
+++ exe/jtc.sh
@@ -4,7 +4,7 @@ BASEDIR=$(dirname `which jtc.sh`)
 
 if [ `uname -o` = "Cygwin" ]
 then
-    python `cygpath -m $BASEDIR/jtc.py "$@"
+    python `cygpath -m $BASEDIR`/jtc.py "$@"
 else
     python ${BASEDIR}/jtc.py "$@"
 fi
